ae4515f3670ce6316e9d252deadaa3866131acda
diederichk <kyle-diederich@uiowa.edu>
2018-07-04 17:29:30 -0500

    Modify ChattyKathy Example
    Works with Firefox only, errors when using with Edge, might require allowing cookies with Chrome.

--- text_to_speech/ChattyKathy/ChattyKathy-master/Example/index.js
+++ text_to_speech/ChattyKathy/ChattyKathy-master/Example/index.js
@@ -1,7 +1,7 @@
 ﻿
 (function () {
     
-    var awsCredentials = new AWS.Credentials("yourAccessKey", "yourSecretAccessKey");
+    var awsCredentials = new AWS.Credentials("AKIAIDL4LA7BGKBBE44Q", "MyE92JoHJoGb6MvpjveaveppWxhUZHCTthoZJ5MJ");
     var settings = {
         awsCredentials: awsCredentials,
         awsRegion: "us-west-2",
@@ -10,8 +10,8 @@
     }
     var kathy = ChattyKathy(settings);
     
-    kathy.Speak("Hello world, my name is Kathy!");
-    kathy.Speak("I can be used for an amazing user experience!");
+    kathy.Speak("<speak>Hello world, my name is Mini-cat! Pleasure to meet you.</speak>");
+    kathy.Speak("<speak><prosody rate=\"slow\"><prosody pitch=\"+20%\"><break time = \"200ms\"/>In some cases, it might help your audience to slow the speaking rate slightly to aid in comprehension.</prosody></prosody></speak>");
 
     if (kathy.IsSpeaking()) {
         kathy.ShutUp(); 
